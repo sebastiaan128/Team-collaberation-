8c30101c65eafb5be164673b90b64e903a5f826b
Sebastiaan Balfoort <581150@edu.rocmn.nl>
2023-12-08 13:19:42 +0100

    style buttons en redirects

--- galgje.php
+++ galgje.php
@@ -1,10 +1,10 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['random'])) {
-        header('Location: ...'); 
+        header('Location: random.php '); 
         exit;
     } elseif (isset($_POST['enter'])) {
-        header('Location: ...'); 
+        header('Location: zelfgekozen.php'); 
         exit;
     }
 }
@@ -17,25 +17,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Galgje</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script></head>
-<body class="bg-primary">
+    <link href="styles.css" rel="stylesheet">
+
+    </head>
+    <body class="bg-info">
     <h1 class="text-center">Galgje</h1>
-    <div class="woord text-center mt-5">
+
+    <div class="woord row d-flex mt-5 w-100 text-center ">
+        <p> Om de beurt raden de spelers een letter. Wanneer de letter niet in het woord voorkomt, zal er een stukje van de 10-delige galg opgebouwd worden. De spelbeurt kan ook gebruikt worden om het juiste woord te raden. Indien dit onjuist was, zal er wederom een stukje van de galg opgebouwd worden.</p>
         <p>Wil je zelf een woord kiezen of een random woord?</p>
     </div>
-    <div class="button d-flex justify-content-center align-items-center py-1">
-        <form method="post">
-            <button type="submit" name="random">Random</button>
+    <div class="button rounded d-flex justify-content-center align-items-center py-1">
+        
+    <form method="post">
+            <button type="submit" name="random" class="rounded">1 player</button>
         </form>
-        <div class="button1 d-flex px-1">
+        <div class="button1 d-flex px-3">
             <form method="post">
-                <button type="submit" name="enter">Zelf kiezen</button>
+                <button type="submit" name="enter" class="rounded ">2 players</button>
             </form>
         </div>
+
     </div>
-</body>
- </html> 
- 
- 
-        <!-- d-flex justify-content-center align-items-center py-1">
-        <input type="text" id="input" placeholder="Voer hier je woord in">
-        <button type="button" onclick="checkWord()">Check</button>  -->
+    </body>
+    </html>
